43a72a4b5d3a3ba95494d3e7de1c2a984234fa43
ermu <168235638@qq.com>
2024-12-19 01:08:01 +0800

    ermu-bookController.查询, 分页显示; 检索, 查询+分页显示

--- src/main/java/com/libmis/controller/BookController.java
+++ src/main/java/com/libmis/controller/BookController.java
@@ -1,10 +1,14 @@
 package com.libmis.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.libmis.entity.Book;
 import com.libmis.service.BookService;
 import com.libmis.utils.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,15 +36,16 @@ public class BookController {
 
     /**
      * 查询所有图书信息(没有考虑分页查询)
+     *
      * @return 所有图书信息，封装在Result 的 data 中
      */
     @GetMapping("/booksList")
     public Result<?> listBooks() {
-        try{
-        List<Book> booksList = bookService.list();
-        log.info("booksList ={}", booksList);
-        return Result.success(booksList);
-        } catch(Exception e){
+        try {
+            List<Book> booksList = bookService.list();
+            log.info("booksList ={}", booksList);
+            return Result.success(booksList);
+        } catch (Exception e) {
             log.error(e.getMessage());
             return Result.error("501", e.getMessage());
         }
@@ -48,15 +53,16 @@ public class BookController {
 
     /**
      * 更新图书信息
-     * @param book  前端发送来的 book 实例
+     *
+     * @param book 前端发送来的 book 实例
      * @return 一个不带数据的 Result
      */
     @PutMapping("/update")
     public Result update(@RequestBody Book book) {
-        try{
-        bookService.saveOrUpdate(book); // 假如没有就新建一个
-        return Result.success();
-        } catch(Exception e){
+        try {
+            bookService.saveOrUpdate(book); // 假如没有就新建一个
+            return Result.success();
+        } catch (Exception e) {
             log.error(e.getMessage());
             return Result.error("501", e.getMessage());
         }
@@ -64,16 +70,17 @@ public class BookController {
 
     /**
      * 添加图书信息
+     *
      * @param book 前端发送来的 book 实例
      * @return 一个不带数据的 Result
      */
     @PostMapping("/save")
     public Result save(@RequestBody Book book) {
         // todo
-        try{
-        bookService.save(book);
-        return Result.success();
-        } catch(Exception e){
+        try {
+            bookService.save(book);
+            return Result.success();
+        } catch (Exception e) {
             log.error(e.getMessage());
             return Result.error("501", e.getMessage());
         }
@@ -81,20 +88,21 @@ public class BookController {
 
     /**
      * 删除一本图书信息
-     * @param bookId  前端发来的bookId
-     * @return  一个不带数据的 Result,
+     *
+     * @param bookId 前端发来的bookId
+     * @return 一个不带数据的 Result,
      */
     @DeleteMapping("/del/{bookId}")
     public Result del(@PathVariable int bookId) {
         // 联表删除的问题
         // *已解决* id不存在时不报错的问题
         try {
-            if (bookService.removeById(bookId)){
+            if (bookService.removeById(bookId)) {
                 return Result.success();
-            }else{
+            } else {
                 return Result.error("501", "哈麻皮你输的bookId不对。");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error(e.getMessage());
             return Result.error("501", e.getMessage());
         }
@@ -103,38 +111,54 @@ public class BookController {
     /**
      * 根据bookId 查询图书信息
      * @param bookId 前端发来的bookId
-     * @return  查询的结果, 一个book实例
+     * @return 查询的结果, 一个book实例
      */
     @GetMapping("/find/{bookId}")
     public Result find(@PathVariable Integer bookId) {
-        try{
-        Book book = bookService.getById(bookId);
-        return Result.success(book);
-        } catch(Exception e){
+        try {
+            Book book = bookService.getById(bookId);
+            return Result.success(book);
+        } catch (Exception e) {
             log.error(e.getMessage());
             return Result.error("501", e.getMessage());
         }
     }
 
+
     /**
-     * 未实现。
-     * 分页查询显示图书信息
-     * @param page 当前页数
-     * @param pageSize 每页的图书信息数
+     * 分页显示图书信息
+     * @param pageNum 当前页数
+     * @param pageSize 每页的图书记录数
      * @return 对应页的图书信息
      */
     @GetMapping("/booksByPage")
-    public Result<?> booksListByPage(@RequestParam(value = "page", defaultValue = "1") Integer page,
-                                     @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize) {
-        // *排序的条件还没有加入
-        System.out.println(page);
-        System.out.println(pageSize);
-        // *排序的条件还没有加入
-        List<Book> booksList = bookService.list();
-        // 返回第pageNo页的数据，为Result
-        List<Book> newList = booksList.subList(pageSize * (page - 1), page * pageSize);
-        System.out.println(newList);
-        return Result.success(newList);
+    public Result<?> bookListByPage(@RequestParam(defaultValue = "1") Integer pageNum,
+                                    @RequestParam(defaultValue = "5")Integer pageSize
+                                    ) {
+        Page<Book> bookPage = bookService.page(new Page<Book>(pageNum, pageSize));
+        log.info("bookPage ={}", bookPage);
+        return Result.success(bookPage);
     }
 
+
+    /**
+     *查询、检索, 分页显示图书信息
+     * @param pageNum 当前页数
+     * @param pageSize 每页的图书记录数
+     * @param search  查询关键词
+     * @return 对应页的图书信息
+     */
+    @GetMapping("/BySearchPage")
+    public Result<?> booksListByConditionPage(@RequestParam(defaultValue = "1")Integer pageNum,
+                                              @RequestParam(defaultValue = "5")Integer pageSize,
+                                              @RequestParam(defaultValue = "")String search) {
+
+        QueryWrapper<Book> queryWrapper = Wrappers.query();
+        if (StringUtils.hasText(search)) {
+            queryWrapper.like("title", search);
+        }
+        Page<Book> page = bookService.page(new Page<>(pageNum, pageSize), queryWrapper);
+        log.info("page ={}", page);
+        return Result.success(page);
+    }
 }
