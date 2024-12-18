788a7c90ff13dd082833812f730c0b4311cff275
ywqtttu <1973614480@qq.com>
2024-12-18 11:45:32 +0800

    12.18兼容性修订by ywqtttu.

--- src/main/java/com/libmis/controller/BookController.java
+++ src/main/java/com/libmis/controller/BookController.java
@@ -4,21 +4,23 @@ import com.libmis.entity.Book;
 import com.libmis.service.BookService;
 import com.libmis.utils.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * @author 二木
  * @date 2024-12-17 22:15
  */
+
 @Slf4j
 @RestController
+@RequestMapping("/testUrl") // 具体url待定
 public class BookController {
 
-    @Resource
+    @Autowired
     private BookService bookService; // 注入 bookService
 
     @GetMapping("/booksList")
@@ -26,5 +28,10 @@ public class BookController {
         List<Book> list = bookService.list();
         log.info("booksList ={}", list);
         return Result.success(list);
+//        return Result.success(null);
+    }
+    @GetMapping("/testTxt")
+    public Result<?> testTxt() {
+        return Result.success("testInfo by ywqtttu.");
     }
 }
